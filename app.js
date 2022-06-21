f994218a576069b40e4808e1731eff6e718dd721
lundbmp <m.lundberg008@gmail.com>
2022-06-20 20:31:14 -0500

    accepts user intput

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [userName, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const pagesHTML = generatePage(userName, github);
 
-fs.writeFile('index.html', generatePage(userName, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pagesHTML, err => {
+//     if (err) throw err;
 
-    console.log('Protfolio complete! Checkout index.html to see the output!');
-});
+//     console.log('Protfolio complete! Checkout index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
